760e63620669ca9541c2fe9adda368e8ef36bc36
dhruv0711 <dhruvraj0711@gmail.com>
2020-08-06 15:01:02 +0530

    updated logo encoded value to local in html

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
+	r.StaticFile("/images/github.png", "./html/images/github.png")
+	r.StaticFile("/images/linkedin.png", "./html/images/linkedin.png")
+	r.LoadHTMLGlob("html/login.html")
 	r.GET("/login", api.Login)
 	r.GET("/githubhome", api.Githubhome)
 	r.GET("/linkedinhome", api.Linkedinhome)
